0432fc75464748f6d5c678cf1dcaae9f901ef597
Graffchik777 <legione77@mail.ru>
2022-07-26 18:36:01 +0300

    add Task1

---
+++ SolutionTask1/Program.cs
@@ -0,0 +1,24 @@
+﻿string? inputlkeOne = Console.ReadLine();
+string? inputlkeTwo = Console.ReadLine();
+
+if(inputlkeOne != null && inputlkeTwo != null)
+{
+    int inputNamberOne = int.Parse(inputlkeOne);
+    int inputNamberTwo = int.Parse(inputlkeTwo);
+
+    //if(inputNamberOne == inputNamberTwo * inputNamberTwo)
+    //{
+    //    Console.WriteLine ("yes");
+    //}
+    //        else{
+    //            Console.WriteLine("No");
+    //        }}
+
+    if(inputNamberTwo == Math.Sqrt(inputNamberOne))
+    {
+        Console.WriteLine ("yes");
+    }
+    else {
+        Console.WriteLine("no");
+    }
+}
