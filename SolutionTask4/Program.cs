ab2047cbeb7fa16c438ace6be231a88222eb6150
Graffchik777 <legione77@mail.ru>
2022-07-26 18:42:22 +0300

    add Task4

---
+++ SolutionTask4/Program.cs
@@ -0,0 +1,29 @@
+﻿// string? inputLine = Console.ReadLine();
+
+// if(inputLine != null)
+// // {
+//     int inputNamber = int.Parse(inputLine);
+
+//     int lineOutputNamber = inputNamber%10;
+
+//     Console.WriteLine(lineOutputNamber);
+//  }
+// {
+//     string lastNum = inputLine.Substring(2,1); 
+
+// Console.WriteLine(lastNum);
+// }
+string? inputLine = Console.ReadLine();
+
+if(inputLine != null)
+{
+    char[] M = inputLine.ToCharArray();
+    Console.WriteLine(M[2]);
+
+
+    // int inputNamber = int.Parse(inputLine);
+
+    // int lineOutputNamber = inputNamber%10;
+
+    
+ }
