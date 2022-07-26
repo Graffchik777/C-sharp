758b12152922dc6c6b52e78d0aee70ecd1227c18
Graffchik777 <legione77@mail.ru>
2022-07-26 18:41:24 +0300

    add Task3

---
+++ SolutionTask3/Program.cs
@@ -0,0 +1,19 @@
+﻿string? inputLkneOne = Console.ReadLine();
+
+if(inputLkneOne != null)
+{
+    int inputNamber = int.Parse(inputLkneOne);
+
+    int startNamber = (inputNamber * -1);
+
+    string lineOutput = "";
+
+    while (startNamber < inputNamber)
+    {
+        lineOutput = lineOutput + startNamber + ", ";
+        startNamber++;
+    }
+
+    lineOutput = lineOutput + inputNamber;
+ Console.WriteLine(lineOutput);
+}
